8aafea3cbf0eb3e7d43e9f1ae5694297372bed18
fnlcon <fnlcon@outlook.com>
2023-08-06 18:16:51 +0800

    update workflow

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-use idl_parser::EndpointParser as EP;
-
-fn main() {
-    let expr = "GET /register/{id:string}/{field:string}?type:string&order:string RQ -> RS";
-    print!("{:?}", EP::parse_endpoint(expr))
-}
